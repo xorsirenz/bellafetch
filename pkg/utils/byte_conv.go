3b5719d410e1c664a402e1eac3d1e1c40eb2761c
madison <69394836+xorsirenz@users.noreply.github.com>
2026-03-19 13:01:28 -0700

    created utils dir for non-specific linux funcs

---
+++ pkg/utils/byte_conv.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"fmt"
+	"math"
+)
+
+func PrettyByteSize(b uint64) string {
+	bf := float64(b)
+	for _, unit := range []string{"", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"} {
+		if math.Abs(bf) < 1024.0 {
+			return fmt.Sprintf("%3.1f%sB", bf, unit)
+		}
+		bf /= 1024.0
+	}
+	return fmt.Sprintf("%.1fYiB", bf)
+}
