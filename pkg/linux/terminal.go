f94eeae4e5d8e3e2798a3c03a5c10d05553e7f70
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-03-29 09:39:28 -0700

    check TERM in env if term program is empty

--- pkg/linux/terminal.go
+++ pkg/linux/terminal.go
@@ -6,7 +6,10 @@ import (
 
 func Terminal() string {
 	if term := os.Getenv("TERM_PROGRAM"); term != "" {
+		return term	
+	} else if term := os.Getenv("TERM"); term != "" {
 		return term
+	} else {
+		return ""
 	}
-	return ""
 }
