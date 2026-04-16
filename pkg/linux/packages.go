af07784255d661d3b44f1efe2bbe305eed91a258
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-04-16 16:52:25 -0700

    removed getid func to its own file

--- pkg/linux/packages.go
+++ pkg/linux/packages.go
@@ -9,30 +9,9 @@ import (
 	"strings"
 )
 
-func parseID(id string) string {
-	ids := strings.Fields(id)
-	firstID := ids[0]
-	cleanedID := strings.ReplaceAll(firstID, "\"", "")
-	id = cleanedID
-	return id
-}
-
-func GetID(osMap map[string]string) string {
-	id := osMap["ID_LIKE"]
-
-	if id == "" {
-		id = osMap["ID"]
-	}
-	if len(id) > 1 {
-		id = parseID(id)
-	}
-	return id
-}
-
-func PkgManager(osMap map[string]string) string {
+func PkgManager(id string) string {
 	flatpaks := flatpak()
 
-	id := GetID(osMap)
 	switch id {
 	case "arch", "manjaro":
 		pkgs := pacman()
@@ -105,7 +84,7 @@ func flatpakRuntimes() int {
 	return count
 }
 
-func rpm() string  {
+func rpm() string {
 	output, err := exec.Command("rpm", "-qa").Output()
 	if err != nil {
 		_ = fmt.Errorf("Error %v", err)
