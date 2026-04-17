be6087406525d5a940d910938dfdd1c57ad605ec
xorsirenz <69394836+xorsirenz@users.noreply.github.com>
2026-04-16 19:17:51 -0700

    moved ascii to txt files and pick from matching config name to file

--- internal/utils/ascii_art.go
+++ internal/utils/ascii_art.go
@@ -1,84 +1,56 @@
 package utils
 
 import (
+	"embed"
+	"io/fs"
 	"strings"
+	"sync"
 )
+
+//go:embed ascii/*
+var asciiFS embed.FS
+
+var asciiCache = map[string]string{}
+var cacheOnce sync.Once
 
 func FetchAscii(id string, config Config) string {
 	asciiMode := strings.ToLower(config.Ascii)
 	id = strings.ToLower(id)
+	loadAsciiFiles()
 
 	if asciiMode == "none" || asciiMode == "disabled" {
-		return AsciiNone()
+		return loadAscii("none.txt")
 	}
 
 	if asciiMode != "default" && asciiMode != "" {
-		switch asciiMode {
-		case "fedora":
-			return AsciiFedora()
-		case "gopher":
-			return AsciiGopher()
-		default:
-			return AsciiNone()
+		if file, ok := asciiCache[asciiMode]; ok {
+			return loadAscii(file)
 		}
+		return loadAscii("none.txt")
 	}
 
-	switch id {
-	case "fedora":
-		return AsciiFedora()
-	case "gopher":
-		return AsciiGopher()
-	default:
-		return AsciiNone()
+	if file, ok := asciiCache[id]; ok {
+		return loadAscii(file)
 	}
+	return loadAscii("none.txt")
 }
 
-func AsciiNone() string {
-	noAscii := ``
-	return noAscii
+func loadAsciiFiles() {
+	cacheOnce.Do(func() {
+		_ = fs.WalkDir(asciiFS, "ascii", func(path string, asciiDir fs.DirEntry, err error) error {
+			if err != nil || asciiDir.IsDir() || !strings.HasSuffix(asciiDir.Name(), ".txt") {
+				return nil
+			}
+			asciiCache[strings.ToLower(strings.TrimSuffix(asciiDir.Name(), ".txt"))] = asciiDir.Name()
+			return nil
+		})
+	})
 }
 
-func AsciiGopher() string {
-	gopher := `
-⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣾⣿⣿⣿⣿⣿⣿⣶⣦⣄⠀⠀⠀⠀⠀⠀
-⠀⠀⠀⠀⢠⡶⣦⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⡴⣦⠀⠀
-⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⣿⣿⣟⠁⠊⣿⣿⣿⣿⣿⡏⠒⠈⣿⣿⣿⡇⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣷⣾⣿⣿⠿⠿⣿⣿⣶⣾⣿⣿⣿⡇⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣷⣤⣤⣿⣿⣿⣿⣿⣿⣿⣧⣼⠛⠀
-⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀
-⠀⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠀⠀⠀
-⠀⠀⠀⠀⠀⠴⡾⠋⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀
-⠀⠀⠀⠀⠀⠀⠀⠀⣹⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⡁⠀⠀⠀⠀⠀
-	`
-	return gopher
+func loadAscii(file string) string {
+	data, err := asciiFS.ReadFile("ascii/" + file)
+	if err != nil {
+		return ""
+	}
+	return string(data)
 }
-
-func AsciiFedora() string {
-	fedora :=`
-             .',;::::;,'.
-         .';:cccccccccccc:;,.
-      .;cccccccccccccccccccccc;.
-    .:cccccccccccccccccccccccccc:.
-  .;ccccccccccccc;$2.:dddl:.$1;ccccccc;.
- .:ccccccccccccc;$2OWMKOOXMWd$1;ccccccc:.
-.:ccccccccccccc;$2KMMc$1;cc;$2xMMc$1;ccccccc:.
-,cccccccccccccc;$2MMM.$1;cc;$2;WW:$1;cccccccc,
-:cccccccccccccc;$2MMM.$1;cccccccccccccccc:
-:ccccccc;$2oxOOOo$1;$2MMM000k.$1;cccccccccccc:
-cccccc;$20MMKxdd:$1;$2MMMkddc.$1;cccccccccccc;
-ccccc;$2XMO'$1;cccc;$2MMM.$1;cccccccccccccccc'
-ccccc;$2MMo$1;ccccc;$2MMW.$1;ccccccccccccccc;
-ccccc;$20MNc.$1ccc$2.xMMd$1;ccccccccccccccc;
-cccccc;$2dNMWXXXWM0:$1;cccccccccccccc:,
-cccccccc;$2.:odl:.$1;cccccccccccccc:,.
-ccccccccccccccccccccccccccccc:'.
-:ccccccccccccccccccccccc:;,..
- ':cccccccccccccccc::;,.
-	`
- return fedora
-}
-
